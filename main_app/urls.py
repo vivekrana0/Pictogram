3282f26ef35a72089f49aba71c4f4136dbb5abf6
Mac <mac@MacdeMacBook-Air.local>
2022-09-18 18:23:11 -0400

    updated base.html home.htm urls.py(for posts_index)

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # General Routes
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    #Posts routes
+    path('posts/', views.posts_index, name='post'),
+    
+    # Signup Route
+
 ]
