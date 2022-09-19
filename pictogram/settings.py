431d6a4578530fa074963a6b7f3e969694496a6f
VivekRana <vivekranarajput@gmail.com>
2022-09-19 10:55:40 -0400

    models

--- pictogram/settings.py
+++ pictogram/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+AUTH_USER_MODEL = 'main_app.User'
+
 WSGI_APPLICATION = 'pictogram.wsgi.application'
 
 
@@ -75,8 +78,8 @@ WSGI_APPLICATION = 'pictogram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pictogram',
     }
 }
 
