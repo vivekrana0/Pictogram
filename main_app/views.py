56725edd964a893247df7ca4119f1e2defabf724
EdwinHawkYu <edwin.hawk.yu@gmail.com>
2022-09-22 16:32:07 -0400

    Increased number of posts pulled from API and aligned posts to the center in explore html

--- main_app/views.py
+++ main_app/views.py
@@ -94,7 +94,7 @@ def explore(request):
   baseurl = "https://api.unsplash.com/search/photos?"
   key = 'CNdf8VEf5G3eoTB71-GPl6XGzDK4xK1NwCeT4is8qBI'
   variable = request.GET.get('explored')
-  image_data = requests.get('{baseurl}query={variable}&client_id={key}'.format(baseurl=baseurl, variable=variable, key=key)).json()
+  image_data = requests.get('{baseurl}per_page=30&query={variable}&client_id={key}'.format(baseurl=baseurl, variable=variable, key=key)).json()
   results = image_data['results']
   return render(request, 'unsplash_api/explore.html', {'results':results})
 
